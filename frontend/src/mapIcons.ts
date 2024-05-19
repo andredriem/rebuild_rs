600a7b777c479e71d62f3cee7a649088a2a46676
André de Araújo Costa Driemeyer <andredriem@gmail.com>
2024-05-18 23:56:30 -0300

    changes

--- frontend/src/mapIcons.ts
+++ frontend/src/mapIcons.ts
@@ -4,6 +4,8 @@ import damSvg2 from './svg/dam-svgrepo-com_2.svg';
 import factorySvg from './svg/factory-svgrepo-com.svg';
 import tunnelSvg from './svg/tunnel-svgrepo-com.svg';
 import channelSvg from './svg/channel-svgrepo-com.svg';
+import questionSvg from './svg/question-and-answer-svgrepo-com.svg';
+
 
 export const mapIcons: Record<string, string> = {
     brickWallWallSvg,
@@ -12,4 +14,5 @@ export const mapIcons: Record<string, string> = {
     factorySvg,
     tunnelSvg,
     channelSvg,
+    questionSvg,
 };
